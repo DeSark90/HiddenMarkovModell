a473b99553de23d199403a8c037330f93b518c82
DeSark90 <s4desark@uni-trier.de>
2017-01-24 20:59:05 +0100

    Methode max wurde angefangen.
    Die Methode max ist wichtig, da hier die Maximale Wahrscheinlichkeit
    berechnet wird indem der Zustandsübergang von statten geht. Die
    Wahrscheinlichkeit eines Bigrams wird mit der Wahrscheinlichkeit der
    Erstellung eines Wortes multipliziert. Um die richtigen
    Wahrscheinlichkeiten miteinander zu multiplizieren, müssen die POS-tags
    vorher miteinander verglichen werden. Wie gilt noch herauszufinden.

--- viterbi/project.clj
+++ viterbi/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot viterbi.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+            :dependencies [[org.clojure/clojure "1.8.0"] [proto-repl "0.3.1"]]
+
+                    :profiles
+                {:dev {:source-paths ["dev" "src" "test"]
+                :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
--- viterbi/src/viterbi/core.clj
+++ viterbi/src/viterbi/core.clj
@@ -1,7 +1,23 @@
-(ns viterbi.core
-  (:gen-class))
+(ns viterbi.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+  ;wordSquence = (hash-map word (hash-map POS probability))
+  (defn tracer
+    "implementation of the viterbi-algorithm"
+   [posMap, biGramMap, wordSequence]
+   (if (contains? wordSequence "<\\s>")
+    wordSequence
+   )
+  )
+
+  (defn maxVal
+    [priMapVa, biMapVal]
+    (max (map (* )))
+    )
+
+  (flatten (map (fn [innereH] (vals innereH)) (vals (hash-map "mein" (hash-map "hund" 12,
+  "kaninchen" 13), "deine" (hash-map "kuh" 14, "katze" 15)))))
